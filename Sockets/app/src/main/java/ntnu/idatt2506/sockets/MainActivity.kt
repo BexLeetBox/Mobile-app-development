d54f60607272b21ebdc61210454713f71587ab4a
Bexter <prisombex@gmail.com>
2023-10-08 14:46:42 +0200

    server implement remaining

--- Sockets/app/src/main/java/ntnu/idatt2506/sockets/MainActivity.kt
+++ Sockets/app/src/main/java/ntnu/idatt2506/sockets/MainActivity.kt
@@ -1,9 +1,11 @@
 package ntnu.idatt2506.sockets
 
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import java.io.DataInputStream
 import java.io.DataOutputStream
@@ -26,19 +28,38 @@ class MainActivity : AppCompatActivity() {
         chatTextView = findViewById(R.id.tv_chat)
         messageEditText = findViewById(R.id.et_message)
         sendButton = findViewById(R.id.btn_send)
+        sendButton.isEnabled = false  //to circumvent race condition bugs
+
+    //-------------------------- thread stuff ------------------------------//
 
         thread {
-            socket = Socket("YOUR_SERVER_IP", 12345) // replace YOUR_SERVER_IP with your server's IP
-            input = DataInputStream(socket.getInputStream())
-            output = DataOutputStream(socket.getOutputStream())
+            try {
+                socket = Socket("localhost", 12345)
+                input = DataInputStream(socket.getInputStream())
+                output = DataOutputStream(socket.getOutputStream())
 
-            while (true) {
-                val message = input.readUTF()
                 runOnUiThread {
-                    chatTextView.append("\nReceived: $message")
+                    sendButton.isEnabled = true
+                }
+
+                while (true) {
+                    val message = input.readUTF()
+                    runOnUiThread {
+                        chatTextView.append("\nReceived: $message")
+                    }
+                }
+            } catch (e: Exception) {
+                e.printStackTrace()
+                runOnUiThread {
+                    // You can provide a more detailed error message based on the exception type.
+                    val errorMsg = "Failed to connect to server: ${e.message}"
+                    Toast.makeText(this@MainActivity, errorMsg, Toast.LENGTH_LONG).show()
+                    Log.e("Socket error", e.toString())
                 }
             }
         }
+
+        //-----------------------------------------------------//
 
         sendButton.setOnClickListener {
             val message = messageEditText.text.toString()
@@ -48,4 +69,17 @@ class MainActivity : AppCompatActivity() {
             }
         }
     }
+
+    //close the socket and streams to free up resources
+    override fun onDestroy() {
+        super.onDestroy()
+        try {
+            input.close()
+            output.close()
+            socket.close()
+        } catch (e: Exception) {
+            // Handle exception if needed
+        }
+    }
+
 }
