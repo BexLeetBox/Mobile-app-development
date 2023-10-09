e82c03169f7e985307a0e655f76e542f5c55a80c
Bexter <prisombex@gmail.com>
2023-10-09 13:44:34 +0200

    works for first message

--- ChatServer/app/src/main/java/ntnu/idatt2506/chatserver/Server.kt
+++ ChatServer/app/src/main/java/ntnu/idatt2506/chatserver/Server.kt
@@ -1,5 +1,6 @@
 package ntnu.idatt2506.chatserver
 
+import android.util.Log
 import android.widget.TextView
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -7,13 +8,16 @@ import kotlinx.coroutines.MainScope
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import java.io.BufferedReader
+import java.io.DataInputStream
 import java.io.IOException
 import java.io.InputStreamReader
 import java.io.PrintWriter
 import java.net.ServerSocket
 import java.net.Socket
 
-class Server(private val textView: TextView, private val port: Int = 12345) {
+private const val TAG = "ChatServerLogs"
+class Server(private val textView: TextView, private val port: Int = 1234) {
+
 
 
     private var clients = mutableListOf<Socket>()
@@ -22,13 +26,18 @@ class Server(private val textView: TextView, private val port: Int = 12345) {
 
     private var ui: String? = ""
         set(str) {
-            MainScope().launch { textView.text = str }
-            field = str
+            MainScope().launch {
+                textView.append("$str\n")
+            }
+            Log.d(TAG, str ?: "null") // Log every ui update
+            field = "$field$str\n"
         }
+
 
     fun start() {
         CoroutineScope(Dispatchers.IO).launch {
             try {
+
                 ui = "Starting Server ..."
                 withContext(Dispatchers.IO) {
                     ServerSocket(port).use { serverSocket ->
@@ -48,15 +57,17 @@ class Server(private val textView: TextView, private val port: Int = 12345) {
                 }
             } catch (e: IOException) {
                 e.printStackTrace()
+                Log.e(TAG, "Server error: ${e.message}") // Log errors
                 ui = e.message
             }
         }
     }
 
     private fun handleClient(clientSocket: Socket) {
-        val reader = BufferedReader(InputStreamReader(clientSocket.getInputStream()))
-        val message = reader.readLine()
+        val dataInputStream = DataInputStream(clientSocket.getInputStream())
+        val message = dataInputStream.readUTF()
         ui = "Client says: $message"
+        Log.d(TAG, "Client says: $message") // Log errors
         broadcast(message)
     }
 
@@ -65,14 +76,17 @@ class Server(private val textView: TextView, private val port: Int = 12345) {
             if (!client.isClosed) {
                 val writer = PrintWriter(client.getOutputStream(), true)
                 writer.println(message)
+                ui = "Sent to ${client.remoteSocketAddress}: $message"
             }
         }
+        Log.d(TAG, "Broadcasting: $message")
         ui = "Broadcasted: $message"
     }
     fun stop() {
         try {
             // Close all client sockets
             for (client in clients) {
+
                 try {
                     client.close()
                 } catch (e: IOException) {
