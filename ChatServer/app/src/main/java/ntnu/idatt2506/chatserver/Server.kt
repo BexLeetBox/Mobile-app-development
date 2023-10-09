2a230f1daacbd03916d055ee11686234dd3e0e2b
Bexter <prisombex@gmail.com>
2023-10-09 16:16:57 +0200

    debugging improvements

--- ChatServer/app/src/main/java/ntnu/idatt2506/chatserver/Server.kt
+++ ChatServer/app/src/main/java/ntnu/idatt2506/chatserver/Server.kt
@@ -39,7 +39,7 @@ class Server(private val textView: TextView, private val port: Int = 1234) {
         CoroutineScope(Dispatchers.IO).launch {
             try {
 
-                ui = "Starting Server ..."
+                ui = "Server started on port: $port"
                 withContext(Dispatchers.IO) {
                     ServerSocket(port).use { serverSocket ->
                         ui = "ServerSocket created, waiting for clients..."
@@ -57,45 +57,55 @@ class Server(private val textView: TextView, private val port: Int = 1234) {
                     }
                 }
             } catch (e: IOException) {
-                e.printStackTrace()
-                Log.e(TAG, "Server error: ${e.message}") // Log errors
-                ui = e.message
+                Log.e(TAG, "Server initialization error", e) // Use exception overload for detailed stack trace
+                ui = "Error initializing server: ${e.localizedMessage}"
             }
         }
     }
 
     private fun handleClient(clientSocket: Socket) {
+        val clientAddress = clientSocket.remoteSocketAddress.toString()
         val dataInputStream = DataInputStream(clientSocket.getInputStream())
         try {
             while (true) {  // Continuously read messages
                 val message = dataInputStream.readUTF()
-                ui = "Client says: $message"
-                Log.d(TAG, "Client says: $message")
+                ui = "Received from $clientAddress: $message"
+                Log.d(TAG, "Received from $clientAddress: $message")
                 broadcast(message)
             }
         } catch (e: EOFException) {
             // Client disconnected or some other read error occurred.
-            Log.d(TAG, "Connection with client ended: ${e.message}")
+            Log.w(TAG, "Connection with client $clientAddress ended: ${e.localizedMessage}") // Using warning level
         } catch (e: IOException) {
             // Handle other IO exceptions
-            Log.e(TAG, "Error reading from client: ${e.message}")
+            Log.e(TAG, "Error reading from client $clientAddress", e) // Detailed stack trace for other IO errors
         }
     }
 
 
     private fun broadcast(message: String) {
         for (client in clients) {
+            val clientAddress = client.remoteSocketAddress.toString()
             if (!client.isClosed) {
-                val writer = PrintWriter(client.getOutputStream(), true)
-                writer.println(message)
-                writer.flush() // Ensure the message is sent immediately
-                ui = "Sent to ${client.remoteSocketAddress}: $message"
+                try {
+                    val writer = PrintWriter(client.getOutputStream(), true)
+                    writer.println(message)
+                    writer.flush() // Ensure the message is sent immediately
+                    Log.d(TAG, "Message sent to $clientAddress: $message")
+                    ui = "Sent to $clientAddress: $message"
+                }catch (e: IOException) {
+                    Log.e(TAG, "Error broadcasting to client $clientAddress", e)
+                }
+            } else {
+                Log.w(TAG, "Skipped closed client $clientAddress during broadcast")
             }
+
         }
-        Log.d(TAG, "Broadcasting: $message")
-        ui = "Broadcasted: $message"
+        Log.i(TAG, "Broadcast completed for message: $message")
     }
+
     fun stop() {
+        Log.i(TAG, "Stopping server and closing connections...")
         try {
             // Close all client sockets
             for (client in clients) {
