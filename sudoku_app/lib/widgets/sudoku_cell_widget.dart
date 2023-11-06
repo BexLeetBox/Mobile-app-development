e5aab5f831a5984facde08f16c94942766c2570b
Bexter <prisombex@gmail.com>
2023-11-06 15:44:52 +0100

    double tapping changes certainty

--- sudoku_app/lib/widgets/sudoku_cell_widget.dart
+++ sudoku_app/lib/widgets/sudoku_cell_widget.dart
@@ -1,13 +1,11 @@
-// sudoku_cell_widget.dart
-
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
-class SudokuCellWidget extends StatelessWidget {
+class SudokuCellWidget extends StatefulWidget {
   final int number;
   final bool isEditable;
   final Function(int?) onSaved; // Callback to save the input number
-  final TextStyle textStyle; // Add this line
+  final TextStyle textStyle;
 
   const SudokuCellWidget({
     Key? key,
@@ -18,46 +16,53 @@ class SudokuCellWidget extends StatelessWidget {
   }) : super(key: key);
 
   @override
+  SudokuCellWidgetState createState() => SudokuCellWidgetState();
+}
+
+class SudokuCellWidgetState extends State<SudokuCellWidget> {
+  bool _isUncertain = false; // State to keep track of uncertainty
+
+  void _toggleUncertain() {
+    setState(() {
+      _isUncertain = !_isUncertain; // Toggle the state
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    // Get the size of the screen or parent widget
-    double width = MediaQuery.of(context).size.width;
-    // Calculate the text size based on the width
-    double textSize = width / 50; // Example calculation
-
-
-    return TextFormField(
-      initialValue: number == 0 ? '' : number.toString(),
-      keyboardType: TextInputType.number,
-      textAlign: TextAlign.center,
-      style: textStyle,
-
-      decoration: const InputDecoration(
-        border: OutlineInputBorder(),
-        hintText: '0',
+    return GestureDetector(
+      onDoubleTap: widget.isEditable ? _toggleUncertain : null, // Handle double tap if editable
+      child: Container(
+        decoration: BoxDecoration(
+          color: _isUncertain ? Colors.red : Colors.white, // Change color based on uncertainty
+          border: Border.all(color: Colors.black38),
+        ),
+        child: TextFormField(
+          initialValue: widget.number == 0 ? '' : widget.number.toString(),
+          keyboardType: TextInputType.number,
+          textAlign: TextAlign.center,
+          style: widget.textStyle.copyWith(color: _isUncertain ? Colors.white : Colors.black), // Change text color based on uncertainty
+          decoration: const InputDecoration(
+            border: InputBorder.none,
+            hintText: '0',
+          ),
+          enabled: widget.isEditable,
+          onSaved: (value) => widget.onSaved(int.tryParse(value ?? '')),
+          inputFormatters: [
+            FilteringTextInputFormatter.allow(RegExp('[1-9]')),
+            LengthLimitingTextInputFormatter(1),
+          ],
+          validator: (value) {
+            if (value == null || value.isEmpty) {
+              return 'Enter a number';
+            }
+            if (int.tryParse(value)! < 1 || int.tryParse(value)! > 9) {
+              return '1-9 only';
+            }
+            return null;
+          },
+        ),
       ),
-
-
-
-      enabled: isEditable,
-      onSaved: (value) => onSaved(int.tryParse(value ?? '')),
-
-      inputFormatters: [
-        FilteringTextInputFormatter.allow(RegExp('[1-9]')), // Allow numbers 1-9 only
-        LengthLimitingTextInputFormatter(1), // Limit input to a single character
-
-      ],
-
-      validator: (value) {
-      // Allow empty strings as a representation of '0'
-        if (value == null || value.isEmpty) {
-          return 'Enter a number'; // Empty field
-        }
-        if (int.tryParse(value)! < 1 || int.tryParse(value)! > 9) {
-          return '1-9 only'; // Out of range number
-        }
-        return null; // No error
-    },
-
     );
   }
 }
